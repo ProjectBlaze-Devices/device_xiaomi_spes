1d8ca99013bb3133e1d728b11e0349d2aeb4c28f
Jabiyeff <cebiyevanar@gmail.com>
2022-12-03 15:21:47 +0400

    spes: hals: Remove custom display commonsys interface
    * Use source one
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -457,7 +457,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    $(LOCAL_PATH)/hals/display/display-commonsys-intf \
     hardware/xiaomi
 
 # Telephony
